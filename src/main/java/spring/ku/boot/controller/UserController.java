5b526b40b668f222867b4bbcc9f0045b0d53dcb9
liushaofei <liushaofei@qujiali.com>
2018-05-15 18:39:19 +0800

    add login

--- src/main/java/spring/ku/boot/controller/UserController.java
+++ src/main/java/spring/ku/boot/controller/UserController.java
@@ -3,6 +3,7 @@ package spring.ku.boot.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import spring.ku.boot.criteria.UserCriteria;
 import spring.ku.boot.model.User;
@@ -22,5 +23,14 @@ public class UserController {
     @GetMapping("/paging")
     public List<User> paging(){
         return userReadService.paging(new UserCriteria());
+    }
+
+    @GetMapping("/login")
+    public Boolean login(@RequestParam("account") String account,
+                         @RequestParam("password") String password){
+        User user = new User();
+        user.setName(account);
+        user.setPassword(password);
+        return userReadService.login(user);
     }
 }
