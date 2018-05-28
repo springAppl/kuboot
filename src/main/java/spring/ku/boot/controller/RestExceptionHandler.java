5017e79121bc3e5243c62d8bae428fd252ddfe06
liushaofei <liushaofei@qujiali.com>
2018-05-28 19:05:16 +0800

    add error

--- src/main/java/spring/ku/boot/controller/RestExceptionHandler.java
+++ src/main/java/spring/ku/boot/controller/RestExceptionHandler.java
@@ -1,19 +1,21 @@
 package spring.ku.boot.controller;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.context.request.WebRequest;
 import spring.ku.boot.exception.ExceptionEntity;
 import spring.ku.boot.exception.WebException;
 
-//@ControllerAdvice
+@ControllerAdvice
 public class RestExceptionHandler {
-    //@ExceptionHandler(WebException.class)
-    protected ResponseEntity<Object> handleInvalidRequest(WebException e, WebRequest request) {
-        HttpStatus httpStatus = HttpStatus.valueOf(500);
+
+    @ExceptionHandler(WebException.class)
+    @ResponseBody
+    protected Object handleInvalidRequest(WebException e, WebRequest request) {
         ExceptionEntity exceptionEntity = new ExceptionEntity();
-        exceptionEntity.setStatus(httpStatus.value());
+        exceptionEntity.setStatus(500);
         exceptionEntity.setMessage(e.getMessage());
-        return new ResponseEntity<>(exceptionEntity, httpStatus);
+        return exceptionEntity;
     }
 }
