88c4cec3737c485b8ed072b00e04f6c87d67e6de
liushaofei <liushaofei@qujiali.com>
2018-07-06 18:23:49 +0800

    add sql

--- src/main/java/spring/ku/boot/controller/TestController.java
+++ src/main/java/spring/ku/boot/controller/TestController.java
@@ -1,5 +1,6 @@
 package spring.ku.boot.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,11 +8,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/test")
 @RestController
 public class TestController {
-//    @Autowired
-//    private LoginUrlAuthenticationEntryPoint loginUrlAuthenticationEntryPoint;
+    @Value("${envName}")
+    private String name;
 
-    @GetMapping("/haha")
-    public String test(){
-        return "good";
+
+    @GetMapping("/name")
+    public String name(){
+        return name;
     }
 }
