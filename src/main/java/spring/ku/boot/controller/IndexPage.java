d5d4e299c25cb9c0e42c4cda3f3f83da1b05ed73
Shaofei Liu <shaofeiliu@ShaofeideMacBook-Pro.local>
2018-08-12 15:42:56 +0800

    add update

--- src/main/java/spring/ku/boot/controller/IndexPage.java
+++ src/main/java/spring/ku/boot/controller/IndexPage.java
@@ -1,27 +1,39 @@
 package spring.ku.boot.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Objects;
 
 @RestController
 @RequestMapping("/api/index")
 public class IndexPage {
 
-//    @Autowired
-//    private Index index;
-
     @Value(value = "classpath:components.json")
     private Resource resource;
 
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
     @GetMapping
     public Object index() throws IOException {
+        Object obj = stringRedisTemplate.opsForValue().get("index");
+        if (Objects.isNull(obj)) {
+            String index = readFromFile();
+            stringRedisTemplate.opsForValue().set("index", index);
+            return index;
+        }
+        return obj;
+    }
+
+
+    private String readFromFile() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(resource.getInputStream()));
         StringBuffer message=new StringBuffer();
         String line = null;
@@ -30,5 +42,11 @@ public class IndexPage {
         }
         String defaultString = message.toString();
         return defaultString;
+    }
+
+
+    @PutMapping
+    public void update(@RequestBody String str){
+        stringRedisTemplate.opsForValue().set("index", str);
     }
 }
